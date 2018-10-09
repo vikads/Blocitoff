0e714f7da85dd4329a37bfca15f76e89bf83e36d
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-10-09 12:16:41 -0700

    3-User-profile-completed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: session[:user_id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
+
 end
